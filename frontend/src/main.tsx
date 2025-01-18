2612a11a6db77f5b81baaa00da20d1b275b1a2f9
santiagoweb2024 <santiagousca716@gmail.com>
2025-01-17 23:40:19 -0500

    feat: integrate ComponentsTest into main application render

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import './styles/tailwind.css';
 import App from './App.tsx';
+import { ComponentsTest } from './pages/Components.test.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ComponentsTest />
   </StrictMode>
 );
