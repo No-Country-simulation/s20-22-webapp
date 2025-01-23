5f604d30944d810880ea3ae9468dd99da85acd87
santiagoweb2024 <santiagousca716@gmail.com>
2025-01-23 00:01:19 -0500

    feat: add RegisterPage route to the application

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { ComponentsTest } from '@pages/Components.test';
 import AuthPage from '@pages/AuthPage';
 import ErrorPage from '@pages/ErrorPage';
 import HomePage from '@pages/HomePage';
+import RegisterPage from '@/pages/RegisterPage';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
@@ -23,6 +24,7 @@ export const router = createBrowserRouter([
         path: 'components',
         element: <ComponentsTest />,
       },
+      { path: 'register', element: <RegisterPage /> },
     ],
   },
 ]);
