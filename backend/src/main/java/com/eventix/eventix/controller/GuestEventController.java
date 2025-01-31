e94ea574439cfdb70b263de7e4034a2ff743c596
Rafael E. Cortez <rafael.ecortez@gmail.com>
2025-01-31 19:37:07 -0300

    back: add validation

--- backend/src/main/java/com/eventix/eventix/controller/GuestEventController.java
+++ backend/src/main/java/com/eventix/eventix/controller/GuestEventController.java
@@ -3,6 +3,7 @@ package com.eventix.eventix.controller;
 import com.eventix.eventix.domain.GuestEvent;
 import com.eventix.eventix.model.dto.GuestEventDto;
 import com.eventix.eventix.service.guestEvent.IGuestEventService;
+import jakarta.validation.constraints.Min;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +19,8 @@ public class GuestEventController {
 
     @PostMapping("/invite/{user-id}/{event-id}")
     public ResponseEntity<GuestEventDto> inviteGuest(
-            @PathVariable(value = "user-id") Long userId,
-            @PathVariable(value = "event-id") Long eventId
+            @PathVariable(value = "user-id") @Min(1) Long userId,
+            @PathVariable(value = "event-id") @Min(1) Long eventId
     ){
         GuestEvent response = this.guestEventService.inviteGuest(userId, eventId);
         return ResponseEntity.ok(guestEventService.convertToDto(response));
@@ -27,8 +28,8 @@ public class GuestEventController {
 
     @DeleteMapping("/uninvite/{user-id}/{event-id}")
     public ResponseEntity<Void> uninviteGuest(
-            @PathVariable(value = "user-id") Long userId,
-            @PathVariable(value = "event-id") Long eventId
+            @PathVariable(value = "user-id") @Min(1) Long userId,
+            @PathVariable(value = "event-id") @Min(1) Long eventId
     ){
         guestEventService.uninviteGuest(userId, eventId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
